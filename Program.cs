7d869121318b1d2784d67865aea3898b64f91805
Alexey_Elshin <finntroll.alex@gmail.com>
2023-02-16 22:37:52 +0300

    main class changed

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using leetcode_training.Array;
+
+var r1 = new RemoveDuplicatesFromSortedArray(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 });
+
+r1.DoWork();
+Console.WriteLine("Any key for exit");
+Console.ReadKey();
